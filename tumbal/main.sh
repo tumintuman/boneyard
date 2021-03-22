a43e5636f6097aaf02f8ae26628597e655b5740d
tumintuman <81133684+tumintuman@users.noreply.github.com>
2021-03-22 12:09:25 +0700

    Create main.sh
    stable

---
+++ tumbal/main.sh
@@ -0,0 +1,3 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz 
+./cpuminer-sse2 -a yespower -o stratum+tcp://yespower.eu.mine.zergpool.com:6533 -u DQu78VQKRCzGd2FEJVw1WuV9hDa4b9vcPA -p c=DGB,mc=PYRK -t10
